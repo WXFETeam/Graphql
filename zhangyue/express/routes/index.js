949bd5b29d2d06fa279524a1470692ab13311180
17621380577 <49052223+17621380577@users.noreply.github.com>
2020-02-25 15:22:04 +0800

    Create index.js

---
+++ zhangyue/express/routes/index.js
@@ -0,0 +1,11 @@
+let express = require('express');
+let router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+router.get('/queryAll', function(req, res, next) {
+    userDao.queryAll(req, res, next);
+});
+module.exports = router;
