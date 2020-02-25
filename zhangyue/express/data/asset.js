9aee20e9642a44aaac44a6e75e0bbea2a507506a
17621380577 <49052223+17621380577@users.noreply.github.com>
2020-02-25 15:20:18 +0800

    Create asset.js

---
+++ zhangyue/express/data/asset.js
@@ -0,0 +1,43 @@
+const AssetsList = [{
+        id: 1,
+        name: 'zhangsan',
+        totalAmount: 6000,
+        assets: {
+            BTC: 1000,
+            ETH: 2000,
+            CYB: 3000
+        }
+    },
+    {
+        id: 2,
+        name: 'lisi',
+        totalAmount: 60000,
+        assets: {
+            BTC: 10000,
+            ETH: 20000,
+            CYB: 30000
+        }
+    },
+    {
+        id: 3,
+        name: 'wangwu',
+        totalAmount: 12010,
+        assets: {
+            BTC: 10,
+            ETH: 2000,
+            CYB: 10000
+        }
+    },
+    {
+        id: 4,
+        name: 'maliu',
+        totalAmount: 450,
+        assets: {
+            BTC: 50,
+            ETH: 100,
+            CYB: 300
+        }
+    }
+];
+
+module.exports = AssetsList;
