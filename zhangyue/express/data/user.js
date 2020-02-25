41ebbb72485ecc8f071b9eed491945365c18366f
17621380577 <49052223+17621380577@users.noreply.github.com>
2020-02-25 15:20:29 +0800

    Create user.js

---
+++ zhangyue/express/data/user.js
@@ -0,0 +1,31 @@
+const UsersList = [{
+        id: 1,
+        name: 'zhangsan',
+        sex: "male",
+        age: 20,
+        totalAmount: 6000
+    },
+    {
+        id: 2,
+        name: 'lisi',
+        sex: "female",
+        age: 20,
+        totalAmount: 60000
+    },
+    {
+        id: 3,
+        name: 'wangwu',
+        sex: "male",
+        age: 20,
+        totalAmount: 12010
+    },
+    {
+        id: 4,
+        name: "maliu",
+        sex: "male",
+        age: 20,
+        totalAmount: 450
+    }
+];
+
+module.exports = UsersList;
